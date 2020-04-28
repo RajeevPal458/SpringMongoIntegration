b51852ebe5607789c742a5898c55adbb4086d725
RajeevPal458 <rajeevkumar.kp100@gmail.com>
2020-04-28 09:54:50 +0900

    initialized spring mongo project

---
+++ src/test/java/com/example/SpringMongoPoject/SpringMongoPojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMongoPoject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoPojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
