9a8c31c5900a205333a42ac956077ba69a73df46
RajeevPal458 <rajeevkumar.kp100@gmail.com>
2020-04-28 09:54:21 +0900

    initialized spring mongo project

---
+++ src/main/java/com/example/SpringMongoPoject/SpringMongoPojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringMongoPoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoPojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoPojectApplication.class, args);
+	}
+
+}
